9f5e05ed2c45427420fb0765c245e71fe6ff0401
ShadyKahouech <shady.kahouech@gmail.com>
2024-06-04 09:34:59 +0100

    done

--- firstNonRepeatedCharacter.js
+++ firstNonRepeatedCharacter.js
@@ -4,11 +4,30 @@
  *  firstNonRepeatedCharacter('ABA'); // => 'B'
  *  firstNonRepeatedCharacter('AACBDB'); // => 'C'
  *  Should return null or empty string of there is no repeated characters
- * 
+ *
  * @function firstNonRepeatedCharacter
- * @param {String} string 
+ * @param {String} string
  * @returns {String}
  */
-const firstNonRepeatedCharacter = function (string) {
-  // TODO: your solution here
+
+function firstNonRepeatedCharacter(string) {
+  return string
+    .split("")
+    .filter(function (character, index, string) {
+      return string.indexOf(character) === string.lastIndexOf(character);
+    })
+    .shift();
+}
+
+function firstNonRepeatedCharacter(string) {
+  var str = string.split("");
+  var char = str[0];
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 1; j < str.length; j++) {
+      if (i !== j && str[j] === str[i]) {
+        char = str[i++];
+      }
+    }
+  }
+  return char;
 }
