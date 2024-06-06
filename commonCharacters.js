90e46978b871dc2c8772541c4a3e777f5ce6cacb
ShadyKahouech <shady.kahouech@gmail.com>
2024-06-06 09:46:39 +0100

    done

--- commonCharacters.js
+++ commonCharacters.js
@@ -20,13 +20,67 @@
 
  **/
 
+// function findCaracter(string1, string2) {
+//   var res = [];
+//   var exist = {};
+//   for (var i = 0; string1.length; i++) {
+//     var char = string1[i];
+//     if (char !== "" && string2.includes(char) && !exist[char]) {
+//       exist[char] = true;
+//       res.push(char);
+//     }
+//   }
+//   return res.join("");
+// }
+
+/**
+
+ *
+
+ * Write a function `f(a, b)` which takes two strings as arguments and returns a
+
+ * string containing the characters found in both strings (without duplication), in the
+
+ * order that they appeared in `a`. Remember to skip spaces and characters you
+
+ * have already encountered!
+
+ *
+
+ * Example: commonCharacters('acexivou', 'aegihobu')
+
+ * Returns: 'aeiou'
+
+ * Should return null or empty string of there is no commonCharacters
+
+ **/
+
 function findCaracter(string1, string2) {
   var res = [];
-
-  for (var j = 0; j < string2.length; j++) {
-    if (string1.includes(string2[j])) {
-      res.push(string2[j]);
+  var exist = {};
+  for (var i = 0; i < string1.lenght; i++) {
+    var char = string1[i];
+    if (char !== " " && string2.includes(char) && !exist[char]) {
+      exist[char] = true;
+      res.push(char);
     }
   }
-  return res;
+
+  return res.join("");
+}
+
+function findCaracter(string1, string2) {
+  var res = "";
+  for (var i = 0; i < string1.length; i++) {
+    for (var j = 0; j < string2.length; j++) {
+      if (
+        string1[i] === string2[j] &&
+        !res.includes(string1[i]) &&
+        string1[i] !== " "
+      ) {
+        res += string1[i];
+      }
+    }
+  }
+  return res || null;
 }
--- isSubsetOf.js
+++ isSubsetOf.js
@@ -3,7 +3,7 @@
  * subset of an input array.  To simplify the problem, you can assume that neither
  * array will contain objects or arrays as elements within them.
  *
- * 
+ *
  * var a = ['commit','push']
  * a.isSubsetOf(['commit','rebase','push','blame']) // true
  *
@@ -11,14 +11,36 @@
  *
  * var b = ['merge','reset','reset']
  *
- * b.isSubsetOf(['reset','merge','add','commit']) // true 
+ * b.isSubsetOf(['reset','merge','add','commit']) // true
  *
  * See http://en.wikipedia.org/wiki/Subset for more on the definition of a
  * subset.
-*/
-Array.prototype.isSubsetOf = function (arr) {
-  // your code here
-
-};
+ */
 
 /*
+/** */
+
+Array.prototype.isSubsetOf = function (arr) {
+  var res = arr;
+  var a = ["commit", "push"];
+  var obj = {};
+
+  for (var i = 0; i < a.length; i++) {
+    for (var j = 0; j < arr.length; j++) {
+      var array = arr[j];
+      var elem = a[i];
+
+      if (
+        !arr.includes(elem) &&
+        !a.includes(array) &&
+        !obj[elem] &&
+        !obj[array]
+      ) {
+        obj[elem] = true;
+        obj[array] = true;
+        res.push(elem, array);
+      }
+    }
+    return res.join(" ") || "";
+  }
+};
